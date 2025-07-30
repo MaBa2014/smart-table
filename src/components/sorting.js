52a51d611874702c00a551bf3737bb81392c823c
MaBa2014 <maba2014@bk.ru>
2025-07-30 09:15:29 +0500

    Первая настройка

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,37 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import { sortCollection, sortMap } from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
-        let field = null;
-        let order = null;
+  let field = null;
+  let order = null;
 
-        if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+  return (data, state, action) => {
+    if (
+      action &&
+      action.name === "sort" &&
+      action.dataset?.field &&
+      action.dataset?.value
+    ) {
+      action.dataset.value = sortMap[action.dataset.value];
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
+      field = action.dataset.field;
+      order = action.dataset.value;
+
+      columns.forEach((col) => {
+        if (col !== action) {
+          col.dataset.value = "none";
         }
-
-        return sortCollection(data, field, order);
+      });
+    } else {
+      columns.forEach((col) => {
+        if (col.dataset.value !== "none") {
+          field = col.dataset.field;
+          order = col.dataset.value;
+        }
+      });
     }
+
+    if (!field || order === "none") return data;
+
+    return sortCollection(data, field, order);
+  };
 }
--- src/main.js
+++ src/main.js
@@ -1,54 +1,81 @@
-import './fonts/ys-display/fonts.css'
-import './style.css'
+import "./fonts/ys-display/fonts.css";
+import "./style.css";
 
-import {data as sourceData} from "./data/dataset_1.js";
+import { data as sourceData } from "./data/dataset_1.js";
+import { initData } from "./data.js";
+import { processFormData } from "./lib/utils.js";
 
-import {initData} from "./data.js";
-import {processFormData} from "./lib/utils.js";
+import { initTable } from "./components/table.js";
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
+import { createComparison, defaultRules } from "./lib/compare.js";
 
-import {initTable} from "./components/table.js";
-// @todo: подключение
+const { data, ...indexes } = initData(sourceData);
 
+let sampleTable;
 
-// Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
-
-/**
- * Сбор и обработка полей из таблицы
- * @returns {Object}
- */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+  const state = processFormData(new FormData(sampleTable.container));
+  const rowsPerPage = parseInt(state.rowsPerPage);
+  const page = parseInt(state.page ?? 1);
 
-    return {
-        ...state
-    };
+  return {
+    ...state,
+    rowsPerPage,
+    page,
+  };
 }
 
-/**
- * Перерисовка состояния таблицы при любых изменениях
- * @param {HTMLButtonElement?} action
- */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
+  let state = collectState();
+  let result = [...data];
 
+  result = applySearching(result, state, action);
+  result = applyFiltering(result, state, action);
+  result = applySorting(result, state, action);
+  result = applyPagination(result, state, action);
 
-    sampleTable.render(result)
+  sampleTable.render(result);
 }
 
-const sampleTable = initTable({
-    tableTemplate: 'table',
-    rowTemplate: 'row',
-    before: [],
-    after: []
-}, render);
+sampleTable = initTable(
+  {
+    tableTemplate: "table",
+    rowTemplate: "row",
+    before: ["search", "header", "filter"],
+    after: ["pagination"],
+  },
+  render
+);
 
-// @todo: инициализация
+const applyFiltering = initFiltering(sampleTable.filter.elements, {
+  searchBySeller: indexes.sellers,
+});
 
+const applyPagination = initPagination(
+  sampleTable.pagination.elements,
+  (el, page, isCurrent) => {
+    const input = el.querySelector("input");
+    const label = el.querySelector("span");
 
-const appRoot = document.querySelector('#app');
+    input.value = page;
+    input.checked = isCurrent;
+    label.textContent = page;
+
+    return el;
+  }
+);
+
+const applySorting = initSorting([
+  sampleTable.header.elements.sortByDate,
+  sampleTable.header.elements.sortByTotal,
+]);
+
+const applySearching = initSearching("search");
+
+const appRoot = document.querySelector("#app");
 appRoot.appendChild(sampleTable.container);
 
 render();
